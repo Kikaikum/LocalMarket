62c609e55bb12ab960195c851980ee8f9d51dc72
Enrique Cabrera Jimenez <90290838+Kikaikum@users.noreply.github.com>
2024-03-11 17:34:44 +0100

    Update postgres.js

--- Backend/libs/postgres.js
+++ Backend/libs/postgres.js
@@ -2,11 +2,11 @@ const { Client } = require('pg');
 
 async function getConnection() {
   const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'nico',
-    password: 'admin123',
-    database: 'zara'
+    host: '0000',
+    port: 0000,
+    user: '0000',
+    password: '0000',
+    database: '0000'
   });
   await client.connect();
   return client;
